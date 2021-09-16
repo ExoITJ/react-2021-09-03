f937d86a39cadac1cd3355a910fe83c6ac4a80a9
Pavlichuk Vlad <exoit2014@yandex.ru>
2021-09-16 13:19:20 +0300

    add delete button

--- src/components/basket/basket.js
+++ src/components/basket/basket.js
@@ -3,7 +3,7 @@ import { connect, useSelector } from 'react-redux';
 import Button from '../button/button';
 import styles from '../product/product.module.css';
 import basketStyles from './basket.module.css';
-import { decrement, increment } from '../../redux/actions';
+import { decrement, increment, remove } from '../../redux/actions';
 
 const Basket = ({ restaurants }) => {
 
@@ -16,8 +16,30 @@ const Basket = ({ restaurants }) => {
   return (
     <div className={basketStyles.basket}>
       <button onClick={show}>Click</button>
+      <div className={styles.buttons}>
+        <Button
+          onClick={decrement}
+          icon="minus"
+          data-id="product-decrement"
+        />
+        <Button
+          onClick={increment}
+          icon="plus"
+          data-id="product-increment"
+        />
+        <Button
+          onClick={remove}
+          icon="plus"
+        />
+      </div>
     </div>
   )
 }
 
-export default connect()(Basket);
+const mapDispatchToProps = (dispatch, props) => ({
+  increment: () => dispatch(increment(props.product.id)),
+  decrement: () => dispatch(decrement(props.product.id)),
+  remove: () => dispatch(remove(props.product.id)),
+});
+
+export default connect(null, mapDispatchToProps)(Basket);
