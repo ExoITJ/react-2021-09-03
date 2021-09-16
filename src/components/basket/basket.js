090a02cc790246d8beea8b9591fb1e26cb605aa0
Pavlichuk Vlad <exoit2014@yandex.ru>
2021-09-16 17:14:05 +0300

    add in basket

--- src/components/basket/basket.js
+++ src/components/basket/basket.js
@@ -1,37 +1,52 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { connect, useSelector } from 'react-redux';
 import Button from '../button/button';
 import styles from '../product/product.module.css';
 import basketStyles from './basket.module.css';
 import { decrement, increment, remove } from '../../redux/actions';
 
-const Basket = ({ restaurants }) => {
+const Basket = () => {
 
   const value = useSelector(state => state.order)
 
   const show = () => {
-    console.log(value);
+    Object.entries(value).map((element) => (
+      console.log(element)
+    ))
   }
+
+  const total = useMemo(() => {
+    return Object.entries(value).reduce((sum, item) => {
+      return sum + item[1] * item[1];
+    }, 0);
+  }, [value]);
 
   return (
     <div className={basketStyles.basket}>
       <button onClick={show}>Click</button>
-      <div className={styles.buttons}>
-        <Button
-          onClick={decrement}
-          icon="minus"
-          data-id="product-decrement"
-        />
-        <Button
-          onClick={increment}
-          icon="plus"
-          data-id="product-increment"
-        />
-        <Button
-          onClick={remove}
-          icon="plus"
-        />
-      </div>
+
+      {Object.entries(value).map((element) => (
+        <div>
+          <span>{element}</span>
+          <div className={styles.buttons}>
+            <Button
+              onClick={decrement}
+              icon="minus"
+              data-id="product-decrement"
+            />
+            <Button
+              onClick={increment}
+              icon="plus"
+              data-id="product-increment"
+            />
+            <Button
+              onClick={remove}
+              icon="plus"
+            />
+          </div>
+        </div>
+      ))}
+      {total}
     </div>
   )
 }
